3152bb2955d138f61c22578b4f2036ea4c11b177
Eric Loew <ericloew@hotmail.com>
2020-02-09 10:53:08 -0600

    base

---
+++ ios/KotlinMppBase/KotlinMppBase/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  KotlinMppBase
+//
+//  Created by Eric Loew on 2/9/20.
+//  Copyright © 2020 erl. All rights reserved.
+//
+
+import UIKit
+import SharedCode
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
+        label.center = CGPoint(x: 160, y: 285)
+        label.textAlignment = .center
+        label.font = label.font.withSize(25)
+        label.text = CommonKt.createApplicationScreenMessage()
+        view.addSubview(label)
+    }
+
+
+}
+
